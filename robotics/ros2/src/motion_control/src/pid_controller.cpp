a0fc21bd056d7b0e23f8f9068e71a02dbc228614
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2022-06-03 02:27:33 +0000

    [FEAT] Defined throttle PID implementation

--- robotics/ros2/src/motion_control/src/pid_controller.cpp
+++ robotics/ros2/src/motion_control/src/pid_controller.cpp
@@ -17,9 +17,32 @@ float PIDController::ThrottlePID(float ref_vx, float cur_vx, double dt)
      * https://www.elprocus.com/the-working-of-a-pid-controller/
      ********************************************/
 
+    // Check if the controller is used or not
+    if (m_throttle_ctrl) {
+        // Check if reference velocity is zero
+        if (ref_vx == 0) {
+            // Reset the integral error
+            m_vx_int_error = 0.0;
+            // Return zero
+            return 0.0;
+        }
+        // Define the error variable
+        float vx_err = ref_vx - cur_vx;
+        // Update integral error
+        m_vx_int_error += vx_err;
+        // Calculate the differential error
+        float diff_err = (vx_err - m_vx_prop_ek1) / dt;
+        // Update the previous error value
+        m_vx_prop_ek1 = vx_err;
+        // Return the control value
+        return m_kp_thr * vx_err + m_ki_thr * m_vx_int_error + m_kd_thr * diff_err;
+    }
+    // Return reference value if controller is not used
+    else {return ref_vx;}
+
     /********************************************
      * END CODE
-     *  ********************************************/
+     *********************************************/
 }
 
 float PIDController::SteeringPID(float ref_wz, float cur_wz, double dt)
@@ -33,6 +56,8 @@ float PIDController::SteeringPID(float ref_wz, float cur_wz, double dt)
      * https://zhuanlan.zhihu.com/p/382010500#:~:text=In%20many%20applications,dynamic%20models%20used.
      * "Combined FeedForward and Feedback Control"
      ********************************************/
+
+
 
     /********************************************
      * END CODE
